9de85825f7c0314e8b22a95ec9d110e3683d5591
Dustin Black <dblack@redhat.com>
2022-12-06 20:36:32 +0100

    update schema to new typing.Annotated model

--- arcaflow_plugin_sysbench/sysbench_schema.py
+++ arcaflow_plugin_sysbench/sysbench_schema.py
@@ -1,31 +1,25 @@
 import typing
-from dataclasses import dataclass, field
-from arcaflow_plugin_sdk import plugin
+from dataclasses import dataclass
+from arcaflow_plugin_sdk import plugin, schema
 
 
 @dataclass
 class CommonInputParameters:
-    threads: typing.Optional[int] = field(
-        default=1,
-        metadata={
-            "name": "Threads",
-            "description": "Number of worker threads to create",
-        },
-    )
-    events: typing.Optional[int] = field(
-        default=0,
-        metadata={
-            "name": "Number of events",
-            "description": "Maximum number of events",
-        },
-    )
-    time: typing.Optional[int] = field(
-        default=10,
-        metadata={
-            "name": "Time",
-            "description": "Limit for total execution time in seconds",
-        },
-    )
+    threads: typing.Annotated[
+        typing.Optional[int],
+        schema.name("Threads"),
+        schema.description("Number of worker threads to create"),
+    ] = 1
+    events: typing.Annotated[
+        typing.Optional[int],
+        schema.name("Number of events"),
+        schema.description("Maximum number of events"),
+    ] = 9
+    time: typing.Annotated[
+        typing.Optional[int],
+        schema.name("Time"),
+        schema.description("Limit for total execution time in seconds"),
+    ] = 10
 
 
 @dataclass
@@ -35,15 +29,13 @@ class SysbenchCpuInputParams(CommonInputParameters):
     input parameters of Sysbench CPU benchmark.
     """
 
-    cpumaxprime: typing.Optional[int] = field(
-        default=10000,
-        metadata={
-            "name": "CPU max prime",
-            "description": (
-                "The upper limit of the number of prime numbers generated"
-            ),
-        },
-    )
+    cpumaxprime: typing.Annotated[
+        typing.Optional[int],
+        schema.name("CPU max prime"),
+        schema.description(
+            "The upper limit of the number of prime numbers generated"
+        ),
+    ] = 10000
 
 
 @dataclass
@@ -53,98 +45,92 @@ class SysbenchMemoryInputParams(CommonInputParameters):
     input parameters of Sysbench Memory benchmark.
     """
 
-    memoryblocksize: typing.Optional[str] = field(
-        default="1KiB",
-        metadata={
-            "name": "Block Size",
-            "description": "size of memory block for test in KiB/MiB/GiB",
-        },
-    )
-    memorytotalsize: typing.Optional[str] = field(
-        default="100G",
-        metadata={
-            "name": "Total Size",
-            "description": "Total size of data to transfer in GiB",
-        },
-    )
-    memoryscope: typing.Optional[str] = field(
-        default="global",
-        metadata={
-            "name": "Memory Scope",
-            "description": "Memory Access Scope(global/local)",
-        },
-    )
-    memoryoperation: typing.Optional[str] = field(
-        default="write",
-        metadata={
-            "name": "Memory Operation",
-            "description": "Type of memory operation(write/read)",
-        },
-    )
+    memoryblocksize: typing.Annotated[
+        typing.Optional[str],
+        schema.name("Block Size"),
+        schema.description("size of memory block for test in KiB/MiB/GiB"),
+    ] = "1KiB"
+    memorytotalsize: typing.Annotated[
+        typing.Optional[str],
+        schema.name("Total Size"),
+        schema.description("Total size of data to transfer in GiB"),
+    ] = "100G"
+    memoryscope: typing.Annotated[
+        typing.Optional[str],
+        schema.name("Memory Scope"),
+        schema.description("Memory Access Scope(global/local)"),
+    ] = "global"
+    memoryoperation: typing.Annotated[
+        typing.Optional[str],
+        schema.name("Memory Operation"),
+        schema.description("Type of memory operation(write/read)"),
+    ] = "write"
 
 
 @dataclass
 class LatencyAggregates:
-    avg: float = field(
-        metadata={"name": "Average", "description": "Average Latency"}
-    )
-    min: float = field(
-        metadata={"name": "Minimum", "description": "Minimum latency"}
-    )
-    max: float = field(
-        metadata={"name": "Maximum", "description": "Maximum Latency"}
-    )
-    P95thpercentile: float = field(
-        metadata={
-            "name": "95th Percentile",
-            "description": "95th percentile latency",
-        }
-    )
-    sum: float = field(
-        metadata={"name": "Sum", "description": "Sum of latencies"}
-    )
+    avg: typing.Annotated[
+        float,
+        schema.name("Average"),
+        schema.description("Average Latency"),
+    ]
+    min: typing.Annotated[
+        float,
+        schema.name("Minimum"),
+        schema.description("Minimum latency"),
+    ]
+    max: typing.Annotated[
+        float,
+        schema.name("Maximum"),
+        schema.description("Maximum Latency"),
+    ]
+    P95thpercentile: typing.Annotated[
+        float,
+        schema.name("95th Percentile"),
+        schema.description("95th percentile latency"),
+    ]
+    sum: typing.Annotated[
+        float,
+        schema.name("Sum"),
+        schema.description("Sum of latencies"),
+    ]
 
 
 @dataclass
 class ThreadFairnessAggregates:
-    avg: float = field(
-        metadata={
-            "name": "Average",
-            "description": "Average value across all threads",
-        }
-    )
-    stddev: float = field(
-        metadata={
-            "name": "Standard Deviation",
-            "description": "Standard deviation of all threads",
-        }
-    )
+    avg: typing.Annotated[
+        float,
+        schema.name("Average"),
+        schema.description("Average value across all threads"),
+    ]
+    stddev: typing.Annotated[
+        float,
+        schema.name("Standard Deviation"),
+        schema.description("Standard deviation of all threads"),
+    ]
 
 
 @dataclass
 class ThreadsFairness:
-    events: ThreadFairnessAggregates = field(
-        metadata={
-            "name": "Thread Fairness events",
-            "description": "number of events executed by the threads ",
-        }
-    )
-    executiontime: ThreadFairnessAggregates = field(
-        metadata={
-            "name": "Thread Fairness execution time",
-            "description": "Execution time of threads",
-        }
-    )
+    events: typing.Annotated[
+        ThreadFairnessAggregates,
+        schema.name("Thread Fairness events"),
+        schema.description("number of events executed by the threads "),
+    ]
+    executiontime: typing.Annotated[
+        ThreadFairnessAggregates,
+        schema.name("Thread Fairness execution time"),
+        schema.description("Execution time of threads"),
+    ]
 
 
 @dataclass
 class CPUmetrics:
-    eventspersecond: float = field(
-        metadata={
-            "name": "Events per second",
-            "description": "Number of events per second to measure CPU speed",
-        }
-    )
+    eventspersecond: typing.Annotated[
+        float,
+        schema.name("Events per second"),
+        schema.description("Number of events per second to measure CPU speed"),
+    ]
 
 
 @dataclass
@@ -154,56 +140,56 @@ class SysbenchMemoryOutputParams:
     parameters returned by sysbench memory benchmark.
     """
 
-    totaltime: float = field(
-        metadata={
-            "name": "Total time",
-            "description": "Total execution time of workload",
-        }
-    )
-    totalnumberofevents: float = field(
-        metadata={
-            "name": "Total number of events",
-            "description": "Total number of events performed by the workload",
-        }
-    )
-    blocksize: str = field(
-        metadata={"name": "Block size", "description": "Block size in KiB"}
-    )
-    totalsize: str = field(
-        metadata={"name": "Total size", "description": "Total size in MiB"}
-    )
-    operation: str = field(
-        metadata={
-            "name": "Operation",
-            "description": "memory operation performed",
-        }
-    )
-    scope: str = field(
-        metadata={"name": "Scope", "description": "scope of operation"}
-    )
-    Totaloperationspersecond: float = field(
-        metadata={
-            "name": "Total operations per second",
-            "description": (
-                "Total number of operations performed by the memory workload"
-                " per second"
-            ),
-        }
-    )
-    Totaloperations: float = field(
-        metadata={
-            "name": "Total operations",
-            "description": (
-                "Total number of operations performed by the memory workload"
-            ),
-        }
-    )
-    Numberofthreads: float = field(
-        metadata={
-            "name": "Number of threads",
-            "description": "Number of threads used by the workload",
-        }
-    )
+    totaltime: typing.Annotated[
+        float,
+        schema.name("Total time"),
+        schema.description("Total execution time of workload"),
+    ]
+    totalnumberofevents: typing.Annotated[
+        float,
+        schema.name("Total number of events"),
+        schema.description("Total number of events performed by the workload"),
+    ]
+    blocksize: typing.Annotated[
+        str,
+        schema.name("Block size"),
+        schema.description("Block size in KiB"),
+    ]
+    totalsize: typing.Annotated[
+        str,
+        schema.name("Total size"),
+        schema.description("Total size in MiB"),
+    ]
+    operation: typing.Annotated[
+        str,
+        schema.name("Operation"),
+        schema.description("memory operation performed"),
+    ]
+    scope: typing.Annotated[
+        str,
+        schema.name("Scope"),
+        schema.description("scope of operation"),
+    ]
+    Totaloperationspersecond: typing.Annotated[
+        float,
+        schema.name("Total operations per second"),
+        schema.description(
+            "Total number of operations performed by the memory workload"
+            " per second"
+        ),
+    ]
+    Totaloperations: typing.Annotated[
+        float,
+        schema.name("Total operations"),
+        schema.description(
+            "Total number of operations performed by the memory workload"
+        ),
+    ]
+    Numberofthreads: typing.Annotated[
+        float,
+        schema.name("Number of threads"),
+        schema.description("Number of threads used by the workload"),
+    ]
 
 
 @dataclass
@@ -213,30 +199,26 @@ class SysbenchCpuOutputParams:
     parameters returned by sysbench cpu benchmark.
     """
 
-    totaltime: float = field(
-        metadata={
-            "name": "Total time",
-            "description": "Total execution time of workload",
-        }
-    )
-    totalnumberofevents: float = field(
-        metadata={
-            "name": "Total number of events",
-            "description": "Total number of events performed by the workload",
-        }
-    )
-    Primenumberslimit: float = field(
-        metadata={
-            "name": "Prime numbers limit",
-            "description": "Number of prime numbers to use for CPU workload",
-        }
-    )
-    Numberofthreads: float = field(
-        metadata={
-            "name": "Number of threads",
-            "description": "Number of threads used by the workload",
-        }
-    )
+    totaltime: typing.Annotated[
+        float,
+        schema.name("Total time"),
+        schema.description("Total execution time of workload"),
+    ]
+    totalnumberofevents: typing.Annotated[
+        float,
+        schema.name("Total number of events"),
+        schema.description("Total number of events performed by the workload"),
+    ]
+    Primenumberslimit: typing.Annotated[
+        float,
+        schema.name("Prime numbers limit"),
+        schema.description("Number of prime numbers to use for CPU workload"),
+    ]
+    Numberofthreads: typing.Annotated[
+        float,
+        schema.name("Number of threads"),
+        schema.description("Number of threads used by the workload"),
+    ]
 
 
 @dataclass
@@ -245,33 +227,29 @@ class SysbenchMemoryResultParams:
     This is the output results data structure for sysbench memory results.
     """
 
-    transferred_MiB: float = field(
-        metadata={
-            "name": "Transferred memory",
-            "description": "Total Memory Transferred",
-        }
-    )
-    transferred_MiBpersec: float = field(
-        metadata={
-            "name": "Transferred memory per second",
-            "description": "Total Memory Transferred per second",
-        }
-    )
-    Latency: LatencyAggregates = field(
-        metadata={
-            "name": "Latency",
-            "description": "Memory Latency in mili seconds",
-        }
-    )
-    Threadsfairness: ThreadsFairness = field(
-        metadata={
-            "name": "Threads fairness",
-            "description": (
-                "Event distribution by threads for number of executed events"
-                " by threads and total execution time by thread"
-            ),
-        }
-    )
+    transferred_MiB: typing.Annotated[
+        float,
+        schema.name("Transferred memory"),
+        schema.description("Total Memory Transferred"),
+    ]
+    transferred_MiBpersec: typing.Annotated[
+        float,
+        schema.name("Transferred memory per second"),
+        schema.description("Total Memory Transferred per second"),
+    ]
+    Latency: typing.Annotated[
+        LatencyAggregates,
+        schema.name("Latency"),
+        schema.description("Memory Latency in mili seconds"),
+    ]
+    Threadsfairness: typing.Annotated[
+        ThreadsFairness,
+        schema.name("Threads fairness"),
+        schema.description(
+            "Event distribution by threads for number of executed events"
+            " by threads and total execution time by thread"
+        ),
+    ]
 
 
 @dataclass
@@ -280,27 +258,24 @@ class SysbenchCpuResultParams:
     This is the output results data structure for sysbench cpu results.
     """
 
-    CPUspeed: CPUmetrics = field(
-        metadata={
-            "name": "CPU speed",
-            "description": "No of events per second",
-        }
-    )
-    Latency: LatencyAggregates = field(
-        metadata={
-            "name": "Latency",
-            "description": "CPU latency in miliseconds",
-        }
-    )
-    Threadsfairness: ThreadsFairness = field(
-        metadata={
-            "name": "Threads fairness",
-            "description": (
-                "Event distribution by threads for number of executed events"
-                " by threads and total execution time by thread"
-            ),
-        }
-    )
+    CPUspeed: typing.Annotated[
+        CPUmetrics,
+        schema.name("CPU speed"),
+        schema.description("No of events per second"),
+    ]
+    Latency: typing.Annotated[
+        LatencyAggregates,
+        schema.name("Latency"),
+        schema.description("CPU latency in miliseconds"),
+    ]
+    Threadsfairness: typing.Annotated[
+        ThreadsFairness,
+        schema.name("Threads fairness"),
+        schema.description(
+            "Event distribution by threads for number of executed events"
+            " by threads and total execution time by thread"
+        ),
+    ]
 
 
 @dataclass
@@ -310,24 +285,22 @@ class WorkloadResultsCpu:
     for the Sysbench CPU success case.
     """
 
-    sysbench_output_params: SysbenchCpuOutputParams = field(
-        metadata={
-            "name": "Sysbench Cpu Output Parameters",
-            "description": (
-                "Ouptut parameters for a successful sysbench cpu workload"
-                " execution"
-            ),
-        }
-    )
-    sysbench_results: SysbenchCpuResultParams = field(
-        metadata={
-            "name": "Sysbench Cpu Result Parameters",
-            "description": (
-                "Result parameters for a successful sysbench cpu workload"
-                " execution"
-            ),
-        }
-    )
+    sysbench_output_params: typing.Annotated[
+        SysbenchCpuOutputParams,
+        schema.name("Sysbench Cpu Output Parameters"),
+        schema.description(
+            "Ouptut parameters for a successful sysbench cpu workload"
+            " execution"
+        ),
+    ]
+    sysbench_results: typing.Annotated[
+        SysbenchCpuResultParams,
+        schema.name("Sysbench Cpu Result Parameters"),
+        schema.description(
+            "Result parameters for a successful sysbench cpu workload"
+            " execution"
+        ),
+    ]
 
 
 @dataclass
@@ -337,24 +310,22 @@ class WorkloadResultsMemory:
     for the Sysbench memory success case.
     """
 
-    sysbench_output_params: SysbenchMemoryOutputParams = field(
-        metadata={
-            "name": "Sysbench Memory Output Parameters",
-            "description": (
-                "Ouptut parameters for a successful sysbench memory workload"
-                " execution"
-            ),
-        }
-    )
-    sysbench_results: SysbenchMemoryResultParams = field(
-        metadata={
-            "name": "Sysbench Memory Result Parameters",
-            "description": (
-                "Result parameters for a successful sysbench Memory workload"
-                " execution"
-            ),
-        }
-    )
+    sysbench_output_params: typing.Annotated[
+        SysbenchMemoryOutputParams,
+        schema.name("Sysbench Memory Output Parameters"),
+        schema.description(
+            "Ouptut parameters for a successful sysbench memory workload"
+            " execution"
+        ),
+    ]
+    sysbench_results: typing.Annotated[
+        SysbenchMemoryResultParams,
+        schema.name("Sysbench Memory Result Parameters"),
+        schema.description(
+            "Result parameters for a successful sysbench Memory workload"
+            " execution"
+        ),
+    ]
 
 
 @dataclass
@@ -363,17 +334,19 @@ class WorkloadError:
     This is the output data structure in the error case.
     """
 
-    exit_code: int = field(
-        metadata={
-            "name": "Exit Code",
-            "description": (
-                "Exit code returned by the program in case of a failure"
-            ),
-        }
-    )
-    error: str = field(
-        metadata={"name": "Failure Error", "description": "Reason for failure"}
-    )
+    exit_code: typing.Annotated[
+        int,
+        schema.name("Exit Code"),
+        schema.description(
+            "Exit code returned by the program in case of a failure"
+        ),
+    ]
+    error: typing.Annotated[
+        str,
+        schema.name("Failure Error"),
+        schema.description("Reason for failure"),
+    ]
+
 
 sysbench_cpu_output_schema = plugin.build_object_schema(
     SysbenchCpuOutputParams
